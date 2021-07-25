f52d653fae911d7b098b05f2f10557409d4a6a82
Gabriel Humeniuc <gabi@work.ro>
2021-07-25 18:41:08 +0300

    Corectat scriptul de instalare

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 DOTFILES="${HOME}/dotfiles"
 
-ln -s -r -f "$DOTFILES/.vim" "${HOME}/.vim"
+ln -s -r -f -T "$DOTFILES/.vim" "${HOME}/.vim"
 ln -s -r -f "$DOTFILES/.bashrc" "${HOME}/.bashrc"
 ln -s -r -f "$DOTFILES/.ctags" "${HOME}/.ctags"
 ln -s -r -f "$DOTFILES/.tmux.conf" "${HOME}/.tmux.conf"
-ln -s -r -f "$DOTFILES/bin" "${HOME}/.local/bin/dotfiles"
+ln -s -r -f -T "$DOTFILES/bin" "${HOME}/.local/bin/dotfiles"
 
