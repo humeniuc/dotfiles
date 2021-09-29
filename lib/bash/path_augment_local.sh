6f0d2710b36b354ce5b16d61f8879c21674fef76
Gabriel Humeniuc <gabi@work.ro>
2021-09-29 16:36:08 +0300

    Deleted debug info

--- lib/bash/path_augment_local.sh
+++ lib/bash/path_augment_local.sh
@@ -2,9 +2,8 @@
 # That way, if I symlink dotfiles/bin to $HOME/.local/bin/dotfiles, the dotfiles/bin executables whould be accesible.
 # PATH_AUGMENT_LOCAL prevents re-augment PATH when starting bash from bash or bash > mc or bash > tmux > bash > mc, etc. 
 function _path_augment_local() {
-    echo "$PATH"
     PATH="${PATH/$PATH_AUGMENT_LOCAL/}"
-    
+
     if [ -d "${HOME}/.local/bin" ]; then
         PATH_AUGMENT_LOCAL="$(find -L "${HOME}/.local/bin" -type d -print0 | sed "s/\x0$//; s/\x0/:/g")"
         PATH="${PATH_AUGMENT_LOCAL}:${PATH}"
