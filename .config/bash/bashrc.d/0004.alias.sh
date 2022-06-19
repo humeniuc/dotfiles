e446ca1693f61a86902b497f01d860dbc55952a5
Gabriel Humeniuc <gabi@work.ro>
2022-06-19 11:39:54 +0300

    alias

--- .config/bash/bashrc.d/0004.alias.sh
+++ .config/bash/bashrc.d/0004.alias.sh
@@ -1,17 +1,18 @@
+alias x1='xargs -n1'
+alias ll='ls -Alt'
+
+[ $(command -v 'setsid') ] && [ $(command -v 'xdg-open') ] && alias xo='setsid xdg-open'
+
 # alias pentru xclip
-[ $(command -v "xclip" 2>/dev/null) ] && alias xc='xclip -selection clipboard'
+[ $(command -v 'xclip' 2>/dev/null) ] && alias xc='xclip -selection clipboard'
 
 [ $(command -v git 2>/dev/null) ] && {
-    alias gs="git status"
-    alias gf="git fetch"
-    alias gprb="git pull --rebase"
+    alias gs='git status'
+    alias gf='git fetch'
+    alias gprb='git pull --rebase'
 
     # fzf giff selector
-    [ $(command -v fzf 2>/dev/null) ] && alias gf='git status -s| sort| fzf --multi | awk '\''{print $2}'\'
+    [ $(command -v fzf 2>/dev/null) ] && alias gfs='git status -s| sort| fzf --multi | awk '\''{print $2}'\'
     # fzf giff diff
-    [ $(command -v fzf 2>/dev/null) ] && alias gfd='git status -s| sort| fzf --multi | awk '\''{print $2}'\'' | xargs -n1 -I{} git diff {}'
+    [ $(command -v fzf 2>/dev/null) ] && alias gfd='gfs | xargs -I{} git diff {}'
 }
-
-alias x1='xargs -n1'
-
-[ $(command -v setsid) ] && [ $(command -v xdg-open) ] && alias xo='setsid xdg-open'
