8cca02351b1e28d4ed16aa6fc581dcb8908f8021
Gabriel Humeniuc <gabi@work.ro>
2023-06-05 20:30:31 +0300

    Refactor tmux sessionizer

--- bin/tmux-sessionizer/tmux-sessionizer.sh
+++ bin/tmux-sessionizer/tmux-sessionizer.sh
@@ -1,69 +1,101 @@
 #!/usr/bin/env bash
 
-if [[ $# -eq 1 ]]; then
-    session_path=$1
-    session_name=$2
-else
-    function _select_path_and_name() {
-        paths_command=( "bash" "$DOTFILES_PATH/bin/tmux-sessionizer/tmux-sessionizer-paths.sh" )
+SESSION_PATH=""
+SESSION_NAME=""
+CMD_ATTACH=()
+CMD_SWITCH=()
 
-        # is termial
-        if [ -t "0" ]; then
-            "${paths_command[@]}" | fzf
+_error()
+{
+    echo "$1" >&2;
+    exit 1;
+}
+
+# Choose a session path 
+# in TERM use fzf, else start rofi/dmenu
+# The options are from tmux-sessionizer-paths.sh
+choose_session_path() {
+    paths_command=( "bash" "$DOTFILES_PATH/bin/tmux-sessionizer/tmux-sessionizer-paths.sh" )
+
+    # is termial
+    if [ -t "0" ]; then
+        "${paths_command[@]}" | fzf
+    else
+        prompt='Choose a project'
+
+        if [ $(command -v rofi) ]; then
+            launcher_cmd=("rofi" "-dmenu" "-i" "-no-custom" "-p" "$prompt" "-monitor" "-4")
         else
-            prompt='Alege un proiect'
-
-            if [ $(command -v rofi) ]; then
-                launcher_cmd=("rofi" "-dmenu" "-i" "-no-custom" "-p" "$prompt" "-monitor" "-4")
-            else
-                launcher_cmd=("dmenu" "-i" "-l" "10" "-p" "${prompt}:" "-fn" "Terminus:regular:size=12")
-            fi
-
-            "${paths_command[@]}" | "${launcher_cmd[@]}"
+            launcher_cmd=("dmenu" "-i" "-l" "10" "-p" "${prompt}:" "-fn" "Terminus:regular:size=12")
         fi
-    }
 
-    < <(_select_path_and_name) read session_path session_name
-fi
-
-# Session path is required.
-[ -z "$session_path" ] && { echo "No session path" >&2; exit 1; }
-
-# Determine session name from path if not set.
-[ -z "$session_name" ] && session_name=$(basename "$session_path" | tr . _)
-
-# Start session if it does not exists
-tmux has-session -t="$selected_name" 2>/dev/null || tmux new-session -d -s "$session_name" -c "$session_path" 
-
-cmd_attach=("tmux" "attach-session" "-t" "$session_name")
-cmd_switch=("tmux" "switch-client" "-t" "$session_name")
-
-# Terminal
-if [ -t "0" ]; then
-    # Switch or attach depending if inside tmux or not
-    [ -n "$TMUX" ] && "${cmd_switch[@]}" || "${cmd_attach[@]}"
-else
-    if ! "${cmd_switch[@]}" 2>/dev/null; then
-        existing_windows=$( xdotool search --onlyvisible --class "$TERMINAL" | sort )
-
-        setsid "$TERMINAL" &
-
-        idx=0
-        while true; do
-            ((idx=idx+1))
-            [ $idx -gt 10 ] && break
-
-            new_windows=$( xdotool search --onlyvisible --class "$TERMINAL" | sort )
-
-            winid=$( comm -13 <( echo "$existing_windows" ) <( echo "$new_windows" ) | tail -n1 )
-
-            if [ ! -z "$winid" ]; then
-                xdotool type --window "$winid" --delay 0 -- "${cmd_attach[*]}" 
-                xdotool key --window "$winid" "Return"
-                break
-            fi
-
-            sleep 0.2
-        done
+        "${paths_command[@]}" | "${launcher_cmd[@]}"
     fi
-fi
+}
+
+
+tmux_switch_or_attach()
+{
+    CMD_ATTACH=("tmux" "attach-session" "-t" "$SESSION_NAME")
+    CMD_SWITCH=("tmux" "switch-client" "-t" "$SESSION_NAME")
+
+    if [ -t "0" ]; then
+        # In terminal: Switch or attach depending if inside tmux or not
+        [ -n "$TMUX" ] && "${CMD_SWITCH[@]}" || "${CMD_ATTACH[@]}"
+    else
+        # Try to switch, else start terminal and attach
+        "${CMD_SWITCH[@]}" 2>/dev/null || tmux_attach_in_new_term
+    fi
+}
+
+
+# start terminal and run the attach command
+tmux_attach_in_new_term()
+{
+    existing_windows=$( xdotool search --onlyvisible --class "$TERMINAL" | sort )
+
+    setsid "$TERMINAL" </dev/null >/dev/null 2>/dev/null &
+
+    idx=0
+    while true; do
+        ((idx=idx+1))
+        [ $idx -gt 10 ] && break
+
+        new_windows=$( xdotool search --onlyvisible --class "$TERMINAL" | sort )
+
+        winid=$( comm -13 <( echo "$existing_windows" ) <( echo "$new_windows" ) | tail -n1 )
+
+        if [ ! -z "$winid" ]; then
+            xdotool type --window "$winid" --delay 0 -- "${CMD_ATTACH[*]}" 
+            xdotool key --window "$winid" "Return"
+            break
+        fi
+
+        sleep 0.2
+    done
+}
+
+
+main() {
+    # path && name could be script params. 
+    # if not choose from options
+    if [[ $# -eq 1 ]]; then
+        SESSION_PATH=$1
+        SESSION_NAME=$2
+    else
+        < <(choose_session_path) read SESSION_PATH SESSION_NAME
+    fi
+
+    # Session path is required.
+    [ -z "$SESSION_PATH" ] && _error "No session path"
+
+    # Determine session name from path if not set.
+    [ -z "$SESSION_NAME" ] && SESSION_NAME=$(basename "$SESSION_PATH" | tr . _)
+
+    # Ensure tmux session
+    tmux has-session -t="$SESSION_NAME" 2>/dev/null || tmux new-session -d -s "$SESSION_NAME" -c "$SESSION_PATH" 
+
+    tmux_switch_or_attach
+}
+
+main "$@"
