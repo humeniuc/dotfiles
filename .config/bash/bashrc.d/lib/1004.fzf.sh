b777f31f3b70130a68514ba4cdf4dcc7afa959de
Gabriel Humeniuc <gabi@work.ro>
2024-01-14 19:55:56 +0200

    include-ul de bash pentru fzf să testeze dacă fișierele există

--- .config/bash/bashrc.d/lib/1004.fzf.sh
+++ .config/bash/bashrc.d/lib/1004.fzf.sh
@@ -1,2 +1,2 @@
-. /usr/share/fzf/completion.bash
-. /usr/share/fzf/key-bindings.bash
+[ -f /usr/share/fzf/completion.bash ] && . /usr/share/fzf/completion.bash
+[ -f /usr/share/fzf/key-bindings.bash ] && . /usr/share/fzf/key-bindings.bash
