b1a283213f966069b6f6628b5fb751684d34d47f
Gabriel Humeniuc <humeniuc@gmail.com>
2024-01-20 12:30:54 +0200

    Bash history control.
    More history lines.
    Append instead of replace.
    Ignore space preceding commands, duplicates, and some comands

--- .config/bash/bashrc.d/0000.base.sh
+++ .config/bash/bashrc.d/0000.base.sh
@@ -6,6 +6,27 @@
 
 source "$DOTFILES_PATH/lib/bash/path_augment_local.sh"
 
+# Large history.
+export HISTSIZE=1000000
+
+# No limit for HISTFILESIZE
+# From: https://excessivelyadequate.com/posts/history.html
+# > If it is unset at this point,
+# > it is set to the same value as `HISTSIZE`. Therefore we must set it to `NIL`,
+# > in which case it isn't "unset", but doesn't have a value either, enabling us
+# > to keep an unlimited history.
+export HISTFILESIZE=""
+
+export HISTTIMEFORMAT="%F %T - "
+
+# Ignores space preceding commands and duplicate commands
+export HISTCONTROL=ignoreboth
+
+# Do not record some commands
+export HISTIGNORE="clear:history:exit:[bf]g:* --help"
+
+shopt -s histappend  # merge session histories
+
 export EDITOR=vim
 
 # What follows next is only for interactive bash
