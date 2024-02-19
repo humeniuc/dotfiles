3c18d02e338c06b3ef8d9ce8b831fabe77b62c41
Gabriel Humeniuc <gabi@work.ro>
2024-02-20 01:15:55 +0200

    .xprofile să înregistreze $TERMINAL

--- .config/xprofile/xprofile.d/0000.base.sh
+++ .config/xprofile/xprofile.d/0000.base.sh
@@ -1,4 +1,7 @@
-export EDITOR=vim
+command -v vim 2>/dev/null && export EDITOR=vim
+command -v xfce4-terminal 2>/dev/null && export TERMINAL=xfce4-terminal
+
+# Add to $DOTFILES/bin to $PATH
 source "$DOTFILES_PATH/lib/bash/path_augment_local.sh"
 
 # background color
