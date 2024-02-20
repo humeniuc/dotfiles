99c119464260433b7e9712c1005d475a67f4d96b
Gabriel Humeniuc <humeniuc@gmail.com>
2024-02-20 21:05:20 +0200

    xprofile gnome-keyring

--- .config/xprofile/xprofile.d/lib/0001.gnome-keyring.sh
+++ .config/xprofile/xprofile.d/lib/0001.gnome-keyring.sh
@@ -1,3 +1,11 @@
-eval $(/usr/bin/gnome-keyring-daemon --start)
-export SSH_AUTH_SOCK
-export SSH_ASKPASS=/usr/lib/ssh/x11-ssh-askpass
+# gnome-keyring already started
+# <https://wiki.archlinux.org/title/GNOME/Keyring#gcr-ssh-agent>
+if [ -S "${XDG_RUNTIME_DIR}/gcr/ssh" ]; then
+    export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/gcr/ssh"
+# TODO: check gnome-keyring-daemon, ssh-agent started by systemd --user
+# manual start
+else
+    eval $(/usr/bin/gnome-keyring-daemon --start)
+    export SSH_AUTH_SOCK
+    export SSH_ASKPASS=/usr/lib/ssh/x11-ssh-askpass
+fi
