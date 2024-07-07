50fc7b55d026e4da42a055d72f84c52884f233b7
Gabriel Humeniuc <humeniuc@gmail.com>
2024-07-07 14:17:21 +0300

    i3wm-xdg-open ca să apelez xdg-open într-un workspace i3wm

--- .config/bash/bashrc.d/0004.alias.sh
+++ .config/bash/bashrc.d/0004.alias.sh
@@ -18,3 +18,16 @@ alias ts='$DOTFILES_PATH/bin/tmux-sessionizer/tmux-sessionizer.sh'
     # fzf giff diff
     [ $(command -v fzf 2>/dev/null) ] && alias gfd='gfs | xargs -I{} git diff {}'
 }
+
+# Alias pentru i3wm-dxg-open cu workspace-ul precompletat
+[ $(command -v i3-msg 2>/dev/null)  -a $(command -v xdg-open 2>/dev/null) ] && {
+    alias xo1="${DOTFILES_PATH}/bin/i3wm-xdg-open 1 "
+    alias xo2="${DOTFILES_PATH}/bin/i3wm-xdg-open 2 "
+    alias xo3="${DOTFILES_PATH}/bin/i3wm-xdg-open 3 "
+    alias xo4="${DOTFILES_PATH}/bin/i3wm-xdg-open 4 "
+    alias xo5="${DOTFILES_PATH}/bin/i3wm-xdg-open 5 "
+    alias xo6="${DOTFILES_PATH}/bin/i3wm-xdg-open 6 "
+    alias xo7="${DOTFILES_PATH}/bin/i3wm-xdg-open 7 "
+    alias xo8="${DOTFILES_PATH}/bin/i3wm-xdg-open 8 "
+    alias xo9="${DOTFILES_PATH}/bin/i3wm-xdg-open 9 "
+} 
